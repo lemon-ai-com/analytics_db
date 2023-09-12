a216e8ea08264038ad4f345ea5af4ed5aeebce3c
Gregory Potemkin <potemkin3940@gmail.com>
2023-09-12 04:04:40 +0300

    upd

--- analytics_db/prepared_data.py
+++ analytics_db/prepared_data.py
@@ -9,7 +9,8 @@ from .connection import Client, add_db_client
 class PreparedDataConnector:
     def __init__(self, pipeline_id: str):
         self.pipeline_id = pipeline_id
-        self.table_path_prefix = f"prepared_data.{self.pipeline_id}"
+        self.table_uservectors = f"prepared_data.`{self.pipeline_id}_uservectors`"
+        self.table_eventvectors = f"prepared_data.`{self.pipeline_id}_eventvectors`"
 
     @add_db_client
     def init_db(self, db_client: Client = None):
@@ -29,8 +30,7 @@ class PreparedDataConnector:
         uservectors_create_columns = [
             f"`{x}` Nullable(Float64)" for x in uservectors.columns if x not in ("user_mmp_id", "install_time")
         ]
-        uservectors_table_name = f"{self.table_path_prefix}_uservectors"
-        create_table_query = f"""CREATE TABLE IF NOT EXISTS {uservectors_table_name} (
+        create_table_query = f"""CREATE TABLE IF NOT EXISTS {self.table_uservectors} (
             user_mmp_id String,
             install_time DateTime,
             {', '.join(uservectors_create_columns)}
@@ -44,8 +44,7 @@ class PreparedDataConnector:
             for x in eventvectors.columns
             if x not in ("user_mmp_id", "event_number", "install_time")
         ]
-        eventvectors_table_name = f"{self.table_path_prefix}_eventvectors"
-        create_table_query = f"""CREATE TABLE IF NOT EXISTS {eventvectors_table_name} (
+        create_table_query = f"""CREATE TABLE IF NOT EXISTS {self.table_eventvectors} (
             user_mmp_id String,
             event_number Int64,
             install_time DateTime,
@@ -56,10 +55,10 @@ class PreparedDataConnector:
         db_client.execute(create_table_query)
 
         db_client.insert_dataframe(
-            f"""INSERT INTO {uservectors_table_name} VALUES""", uservectors
+            f"""INSERT INTO {self.table_uservectors} VALUES""", uservectors
         )
         db_client.insert_dataframe(
-            f"""INSERT INTO {eventvectors_table_name} VALUES""", eventvectors
+            f"""INSERT INTO {self.table_eventvectors} VALUES""", eventvectors
         )
 
     @add_db_client
@@ -82,7 +81,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT *
-        FROM {self.table_path_prefix}_uservectors
+        FROM {self.table_uservectors}
         {('WHERE' + ' AND '.join(where_parts)) if len(where_parts) > 0 else ''}
         """
 
@@ -90,7 +89,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT *
-        FROM {self.table_path_prefix}_eventvectors
+        FROM {self.table_eventvectors}
         {('WHERE' + ' AND '.join(where_parts)) if len(where_parts) > 0 else ''}
         """
 
@@ -118,7 +117,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT uniq(user_mmp_id) as result
-        FROM {self.table_path_prefix}_eventvectors
+        FROM {self.table_eventvectors}
         {('WHERE' + ' AND '.join(where_parts)) if len(where_parts) > 0 else ''}
         """
         return db_client.query_dataframe(query).iloc[0, 0]
@@ -143,7 +142,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT count(1) as result
-        FROM {self.table_path_prefix}_eventvectors
+        FROM {self.table_eventvectors}
         {('WHERE' + ' AND '.join(where_parts)) if len(where_parts) > 0 else ''}
         """
         return db_client.query_dataframe(query).iloc[0, 0]
@@ -167,7 +166,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT date_trunc('hour', install_time) as install_hour, count(1) as number_of_events
-        FROM {self.pipeline_id}_eventvectors
+        FROM {self.table_eventvectors}
         {('WHERE' + ' AND '.join(where_parts)) if len(where_parts) > 0 else ''}
         GROUP BY install_hour
         """
@@ -195,7 +194,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT uniq(toDate(install_time)) as result
-        FROM {self.table_path_prefix}_uservectors
+        FROM {self.table_uservectors}
         {('WHERE' + ' AND '.join(where_parts)) if len(where_parts) > 0 else ''}
         """
 
@@ -222,7 +221,7 @@ class PreparedDataConnector:
 
         query = f"""
         SELECT min(install_time) as result
-        FROM {self.table_path_prefix}_uservectors
+        FROM {self.table_uservectors}
         WHERE user_mmp_id NOT IN (
             SELECT user_mmp_id
             FROM predict.{model_id}_predict
